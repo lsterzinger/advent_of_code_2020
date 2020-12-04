09d4db18974d10b9c1c21713fc5782e1f6da5782
Lucas Sterzinger <lsterzinger@ucdavis.edu>
2020-12-04 09:14:31 -0800

    Functionized, but still not working

--- 04/04.jl
+++ 04/04.jl
@@ -1,41 +1,48 @@
 using DelimitedFiles
 using Printf
 
-open("./input.dat", "r") do f
-    total = 0
-    inval = 0
-    dict = Dict()
 
-    req_fields = [
-        "byr",
-        "iyr",
-        "eyr",
-        "hgt",
-        "hcl",
-        "ecl",
-        "pid"
-    ]
+function test_passports(file, req_fields)
+    open(file, "r") do f
+        total = 0
+        inval = 0
+        dict = Dict()
 
-    for l in eachline(f)
-        if l == ""
-            for key in req_fields
-                if haskey(dict, key) == false
-                    inval += 1
-                    break
+        for l in eachline(f)
+            if l == ""
+                total += 1
+                for key in req_fields
+                    if haskey(dict, key) == false
+                        inval += 1
+                        break
+                    end
+                end
+                dict = Dict()
+            else
+                for string in split(l, " ")
+                    pair = split(string, ":")
+                    # println(pair)
+                    dict[pair[1]] = pair[2]
                 end
             end
-            total += 1
-            dict = Dict()
-        else
-            for string in split(l, " ")
-                pair = split(string, ":")
-                dict[pair[1]] = pair[2]
-            end
         end
+        println(file)
+        println("Total\t",total)
+        println("Invalid\t",inval)
+        println("Valid\t",total-inval)
     end
-    println(total)
-    println(inval)
-    println(total-inval)
 end
 
 # print("Total\t$(total)\nInvalid\t$(inval)\n")
+req_fields = [
+    "byr", 
+    "iyr", 
+    "eyr", 
+    "hgt",     
+    "hcl", 
+    "ecl", 
+    "pid", 
+]
+
+test_passports("./input.dat", req_fields)
+test_passports("./test_input.txt", req_fields)
