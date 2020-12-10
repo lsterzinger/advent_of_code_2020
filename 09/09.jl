4696bf0a8682e1561bf2617cbd1318dc55f07407
Lucas Sterzinger <lsterzinger@ucdavis.edu>
2020-12-09 16:37:31 -0800

    Finished part 2

--- 09/09.jl
+++ 09/09.jl
@@ -31,8 +31,37 @@ function check_vals(file; preamble = 25)
     end
 end
 
+# Function to find the weakness
+# Defined as the sum of the min and max
+# values of a continuous section of code
+# that sum to the invalid value
+function find_weakness(file, val)
+    i = 1
+    di = 1
+    
+    # Do forever!
+    while true
+        j = i + di
+        arr = file[i:j]
+        s = sum(arr)
+
+        if s == val
+            return minimum(arr) + maximum(arr)
+        elseif s > val
+            i += 1
+            di = 1
+        else
+            di += 1
+        end
+    end
+     
+end
+
+# Test values
 test_vals = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576,]
 
 data =  parse.(Int, readlines("./XMAS_data.txt"))
 println("Test: ", check_vals(test_vals, preamble=5))
-println("Real: ", check_vals(data))
+inval = check_vals(data)
+println("Real: ", inval)
+println("Encryption weakness: ", find_weakness(data, inval))
